0b101df609d60f58f4a7292293dd98bf42ef77fa
oldyear <2811254287@qq.com>
2024-08-15 18:43:28 +0800

    建库

---
+++ src/test/java/com/example/dlquerybackend/DlQueryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dlquerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlQueryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
